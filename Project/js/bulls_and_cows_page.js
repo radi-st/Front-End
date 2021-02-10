609af19cab0a13e8bddf5115e3b07c7b6467732c
Radina Stoyanova <radi.stoyano.55@abv.bg>
2021-02-10 23:35:40 +0200

    TODO:scores

--- Project/js/bulls_and_cows_page.js
+++ Project/js/bulls_and_cows_page.js
@@ -138,18 +138,50 @@ function afterGuess(number) {
     img.style.maxWidth = "150px";
     img.style.maxHeight = "auto";
   }
-
+  
   for (let i = 1; i <= cows; i++) {
     let img = document.createElement("IMG");
     img.setAttribute("src", "./resources/cow.gif");
-    animals.appendChild(img);
+    animals.appendChild(img)
+    
   }
 
+  function congratsFunc()
+  {
+    let body =document.getElementsByTagName("body")[0];
+    document.getElementsByTagName("header")[0].style.display="none";
+    document.getElementsByClassName("game-content")[0].style.display="none";
+    let img = document.createElement("img");
+    img.setAttribute("src","./resources/cow_flowers.gif");
+    body.appendChild(img);
+    img.style.width="300px"
+    img.style.maxHeight = "auto";
+    body.style.alignItems="center";
+    body.style.display="flex";
+    body.style.flexDirection="column";
+    let congrats_text = document.createElement("p");
+    congrats_text.innerText="Congratulations! You have guessed the number!";
+    congrats_text.style.fontSize="3.5rem";
+    congrats_text.style.color="white";
+    congrats_text.style.backgroundColor="black";
+    congrats_text.style.padding="10px";
+    body.appendChild(congrats_text);
+    let play_again = document.createElement("button");
+    play_again.classList.add("start-game");
+    play_again.innerText="Play Again";
+    play_again.setAttribute("onClick","window.location.reload();");
+    body.appendChild(play_again);
+  }
+
+  if(bulls===4)
+  {
+    setTimeout(congratsFunc, 1000);
+  }
   //alert(`Bulls: ${bulls}; Cows: ${cows}`);
 
 }
 
-var numberToGuess;
-do {
+var numberToGuess=1234;
+/*do {
   numberToGuess = Math.floor(1000 + Math.random() * 9000);
-} while (!different4Digits(numberToGuess));
+} while (!different4Digits(numberToGuess));*/
